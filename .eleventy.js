0e75010d1caba8eda8d735b19084a385e1ff6634
Julián Castro <juliancastrofiorito@gmail.com>
2024-02-05 11:43:50 +0100

    cambiar imaxes

--- .eleventy.js
+++ .eleventy.js
@@ -12,8 +12,8 @@ module.exports = function (eleventyConfig) {
 
   // human readable date
   eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
-      "dd LLL yyyy"
+    return DateTime.fromJSDate(dateObj, { zone: "Europe/Madrid", locale: 'gl'  }).toFormat(
+      "dd LLLL yyyy"
     );
   });
 
